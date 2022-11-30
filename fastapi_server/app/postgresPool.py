c80705d394c5986705c4852dacdea34636447af5
Petter Lauvrak <pettelau@stud.ntnu.no>
2022-11-30 19:17:37 +0100

    uncomment pool

--- fastapi_server/app/postgresPool.py
+++ fastapi_server/app/postgresPool.py
@@ -3,11 +3,11 @@ from psycopg2 import pool
 from .credentials import Credentials
 
 
-# pool = pool.SimpleConnectionPool(
-#     1,
-#     20,
-#     host=Credentials.host,
-#     database=Credentials.db,
-#     user=Credentials.user,
-#     password=Credentials.password,
-# )
+pool = pool.SimpleConnectionPool(
+    1,
+    20,
+    host=Credentials.host,
+    database=Credentials.db,
+    user=Credentials.user,
+    password=Credentials.password,
+)
