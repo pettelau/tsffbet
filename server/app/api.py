c071160b2000f1918b1c3a6435fd8a85939d3c45
Petter Lauvrak <pettelau@stud.ntnu.no>
2023-08-24 16:32:56 +0200

    try except

--- server/app/api.py
+++ server/app/api.py
@@ -860,55 +860,60 @@ async def add_player(player: dict):
 
 @app.get("/api/bonde/games")
 async def get_users():
-    # try:
-    query = "SELECT * FROM games ORDER BY game_id DESC;"
-    games = fetchDBJsonNew(query)
+    try:
+        query = "SELECT * FROM games ORDER BY game_id DESC;"
+        games = fetchDBJsonNew(query)
 
-    # Loop through each game and get the players for that game
-    for game in games:
-        players_query = """
-            SELECT game_players.player_id, game_player_id, nickname, score
-            FROM games 
-            NATURAL JOIN game_players 
-            LEFT JOIN bonde_users ON game_players.player_id = bonde_users.player_id 
-            WHERE game_id = %s;
-        """
-        players_result = fetchDBJsonNew(players_query, (game["game_id"],))
-        game["players"] = [
-            {"nickname": player["nickname"], "score": player["score"]}
-            for player in players_result
-        ]
+        # Loop through each game and get the players for that game
+        # if games:
+        for game in games:
+            players_query = """
+                SELECT game_players.player_id, game_player_id, nickname, score
+                FROM games 
+                NATURAL JOIN game_players 
+                LEFT JOIN bonde_users ON game_players.player_id = bonde_users.player_id 
+                WHERE game_id = %s;
+            """
+            players_result = fetchDBJsonNew(players_query, (game["game_id"],))
+            game["players"] = [
+                {"nickname": player["nickname"], "score": player["score"]}
+                for player in players_result
+            ]
 
-    # except Exception as e:
-    #     return HTTPException(
-    #         status_code=500, detail="Something wrong. Could not fetch games"
-    #     )
+    except Exception as e:
+        return HTTPException(
+            status_code=500, detail="Something wrong. Could not fetch games"
+        )
     return {"games": games}
 
 
 @app.get("/api/bonde/game/{game_id}")
 async def get_game(game_id: int):
-    query = "SELECT * FROM games WHERE game_id = %s;"
-    game = fetchDBJsonNew(query, (game_id,))
+    try:
+        query = "SELECT * FROM games WHERE game_id = %s;"
+        game = fetchDBJsonNew(query, (game_id,))
 
-    # Fetch corresponding rounds
-    rounds_query = "SELECT round_id, num_cards, dealer_index, locked FROM rounds WHERE game_id = %s;"
-    rounds = fetchDBJsonNew(rounds_query, (game_id,))
+        # Fetch corresponding rounds
+        rounds_query = "SELECT round_id, num_cards, dealer_index, locked FROM rounds WHERE game_id = %s;"
+        rounds = fetchDBJsonNew(rounds_query, (game_id,))
 
-    for round in rounds:
-        round_scores_qurey = "SELECT player_scores_id, num_tricks, stand FROM player_scores WHERE round_id = %s ORDER BY player_scores_id ASC;"
-        round_scores = fetchDBJsonNew(round_scores_qurey, (round["round_id"],))
-        round["player_scores"] = round_scores
+        for round in rounds:
+            round_scores_qurey = "SELECT player_scores_id, num_tricks, stand FROM player_scores WHERE round_id = %s ORDER BY player_scores_id ASC;"
+            round_scores = fetchDBJsonNew(round_scores_qurey, (round["round_id"],))
+            round["player_scores"] = round_scores
 
-    players_query = """
-            SELECT nickname, game_player_id, game_players.player_id 
-            FROM games 
-            NATURAL JOIN game_players 
-            LEFT JOIN bonde_users ON game_players.player_id = bonde_users.player_id 
-            WHERE game_id = %s ORDER BY game_player_id ASC;
-        """
-    players = fetchDBJsonNew(players_query, (game_id,))
-
+        players_query = """
+                SELECT nickname, game_player_id, game_players.player_id 
+                FROM games 
+                NATURAL JOIN game_players 
+                LEFT JOIN bonde_users ON game_players.player_id = bonde_users.player_id 
+                WHERE game_id = %s ORDER BY game_player_id ASC;
+            """
+        players = fetchDBJsonNew(players_query, (game_id,))
+    except Exception as e:
+        return HTTPException(
+            status_code=500, detail="Something wrong. Could not fetch games"
+        )
     return {"game": game, "rounds": rounds, "players": players}
 
 
@@ -926,27 +931,27 @@ async def get_users():
 
 @app.post("/api/bonde/game")
 async def create_game(game: GameCreate):
-    # try:
-    cursor = connection.cursor()
-    query1 = "INSERT INTO games(money_multiplier, extra_cost_loser, extra_cost_second_last) VALUES (%s, %s, %s) RETURNING game_id;"
-    cursor.execute(
-        query1,
-        (game.money_multiplier, game.extra_cost_loser, game.extra_cost_second_last),
-    )
-    game_id = cursor.fetchone()[0]
+    try:
+        cursor = connection.cursor()
+        query1 = "INSERT INTO games(money_multiplier, extra_cost_loser, extra_cost_second_last) VALUES (%s, %s, %s) RETURNING game_id;"
+        cursor.execute(
+            query1,
+            (game.money_multiplier, game.extra_cost_loser, game.extra_cost_second_last),
+        )
+        game_id = cursor.fetchone()[0]
 
-    game_player_ids = []
-    # Add players to game_players table
-    for player_id in game.players:
-        query = "INSERT INTO game_players(game_id, player_id) VALUES (%s, %s) RETURNING game_player_id;"
-        cursor.execute(query, (game_id, player_id))
-        game_player_ids.append(cursor.fetchone()[0])
+        game_player_ids = []
+        # Add players to game_players table
+        for player_id in game.players:
+            query = "INSERT INTO game_players(game_id, player_id) VALUES (%s, %s) RETURNING game_player_id;"
+            cursor.execute(query, (game_id, player_id))
+            game_player_ids.append(cursor.fetchone()[0])
 
-    connection.commit()
-    # except Exception as e:
-    #     return HTTPException(
-    #         status_code=500, detail="Something wrong. Could not create game"
-    #     )
+        connection.commit()
+    except Exception as e:
+        return HTTPException(
+            status_code=500, detail="Something wrong. Could not create game"
+        )
 
     return {
         "game_id": game_id,
@@ -992,36 +997,36 @@ async def create_rounds(rounds: dict):
 @app.put("/api/bonde/rounds")
 async def update_round(data: dict):
     # Connect to the database
-    # try:
-    cursor = connection.cursor()
+    try:
+        cursor = connection.cursor()
 
-    # # Update the rounds table
-    # cursor.execute(
-    #     "UPDATE rounds SET num_cards = %s, dealer_index = %s, locked = %s WHERE round_id = %s",
-    #     (
-    #         roundData["num_cards"],
-    #         roundData["dealer_index"],
-    #         roundData["locked"],
-    #         round_id,
-    #     ),
-    # )
-    # Update player_scores
-    print(data)
-    for round in data["rounds"]:
-        for player_score in round["player_scores"]:
-            cursor.execute(
-                "UPDATE player_scores SET num_tricks = %s, stand = %s WHERE player_scores_id = %s",
-                (
-                    player_score["num_tricks"],
-                    player_score["stand"],
-                    player_score["player_scores_id"],
-                ),
-            )
-    connection.commit()
-    # except Exception as e:
-    #     return HTTPException(
-    #         status_code=500, detail="Something wrong. Could not update round"
-    #     )
+        # # Update the rounds table
+        # cursor.execute(
+        #     "UPDATE rounds SET num_cards = %s, dealer_index = %s, locked = %s WHERE round_id = %s",
+        #     (
+        #         roundData["num_cards"],
+        #         roundData["dealer_index"],
+        #         roundData["locked"],
+        #         round_id,
+        #     ),
+        # )
+        # Update player_scores
+        print(data)
+        for round in data["rounds"]:
+            for player_score in round["player_scores"]:
+                cursor.execute(
+                    "UPDATE player_scores SET num_tricks = %s, stand = %s WHERE player_scores_id = %s",
+                    (
+                        player_score["num_tricks"],
+                        player_score["stand"],
+                        player_score["player_scores_id"],
+                    ),
+                )
+        connection.commit()
+    except Exception as e:
+        return HTTPException(
+            status_code=500, detail="Something wrong. Could not update round"
+        )
 
     return {"message": "Round updated successfully"}
 
@@ -1029,27 +1034,27 @@ async def update_round(data: dict):
 @app.put("/api/bonde/playerdata")
 async def update_player_scores(data: dict):
     # Connect to the database
-    # try:
-    print(data)
-    cursor = connection.cursor()
+    try:
+        print(data)
+        cursor = connection.cursor()
 
-    # Update player_scores
-    for player in data["playerData"]:
-        try:
-            cursor.execute(
-                "UPDATE game_players SET score = %s WHERE game_player_id = %s",
-                (
-                    player["score"],
-                    player["game_player_id"],
-                ),
-            )
-        except KeyError:
-            print("KeyError")
-    connection.commit()
-    # except Exception as e:
-    #     return HTTPException(
-    #         status_code=500, detail="Something wrong. Could not update player scores"
-    #     )
+        # Update player_scores
+        for player in data["playerData"]:
+            try:
+                cursor.execute(
+                    "UPDATE game_players SET score = %s WHERE game_player_id = %s",
+                    (
+                        player["score"],
+                        player["game_player_id"],
+                    ),
+                )
+            except KeyError:
+                print("KeyError")
+        connection.commit()
+    except Exception as e:
+        return HTTPException(
+            status_code=500, detail="Something wrong. Could not update player scores"
+        )
 
     return {"message": "Player scores updated successfully"}
 
